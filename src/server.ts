9ccef3ce70245741e04347ba572aa4a8a9ae87c9
oiviana <vianalucas80@gmail.com>
2024-06-30 17:13:44 -0300

    fix: using middleware in server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
+import app from "./app";
 
-const app = express();
+
 const port = process.env.SERVER_PORT ?? 3000;
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
