1cce76d201f3eaf1204ace7975663e8228247060
oiviana <vianalucas80@gmail.com>
2024-06-30 17:02:52 -0300

    refactor: server port

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
 const app = express();
+const port = process.env.SERVER_PORT ?? 3000;
 
-app.listen(3333, () => console.log("server is running on port 3333"));
+app.listen(port, () => console.log(`server is running on port ${port}`));
