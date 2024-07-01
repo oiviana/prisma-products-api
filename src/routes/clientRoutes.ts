bfa5ffd23cc67c96b5161189b4e09d099f177e7d
oiviana <vianalucas80@gmail.com>
2024-06-30 21:49:11 -0300

    fix: removing test route

--- src/routes/clientRoutes.ts
+++ src/routes/clientRoutes.ts
@@ -5,11 +5,6 @@ const clientRoutes = express.Router();
 
 clientRoutes.post("/create", createClient);
 
-clientRoutes.get('/teste', (req, res)=>{
-
-    return res.send("Servidor na rota de teste")
-})
-
 clientRoutes.get("/list-clients", listClients )
 
 clientRoutes.get("/find-client/:id", findClient )
