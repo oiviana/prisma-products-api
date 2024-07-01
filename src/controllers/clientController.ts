7c81780c08974821ac586a41e7a2d3f3543d068b
oiviana <vianalucas80@gmail.com>
2024-07-01 20:09:40 -0300

    feat: client crud

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
 
+// POST
 export const createClient = async (req: Request, res: Response) => {
   const { clientCnpj, clientName, clientEmail } = req.body;
   try {
@@ -22,12 +23,13 @@ export const createClient = async (req: Request, res: Response) => {
   }
 };
 
+// GET
 export const listClients = async (_req: Request, res: Response) => {
   try {
     const clients = await prisma.client.findMany();
     res.json(clients);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao cadastrar: ${error}` });
+    res.status(500).json({ error: `Erro ao listar: ${error}` });
   } finally {
     await prisma.$disconnect();
   }
@@ -51,6 +53,39 @@ export const findClient = async (req: Request, res: Response) => {
   } catch (error) {
     console.error("Erro ao buscar cliente:", error);
     res.status(500).json({ error: `Erro ao buscar cliente: ${error}` });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// DELETE
+
+export const deleteClient = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const client = await prisma.client.findUnique({
+      where: {
+        clientId: id,
+      },
+    });
+
+    if (!client) {
+      return res.status(404).json({ error: "Cliente não encontrado" });
+    }
+
+    await prisma.address.deleteMany({
+      where: { clientId: id },
+    });
+
+    await prisma.client.delete({
+      where: { clientId: id },
+    });
+
+    res.json("Cliente deletado");
+  } catch (error) {
+    console.error("Erro ao deletar cliente:", error);
+    res.status(500).json({ error: `Erro ao deletar cliente: ${error}` });
   } finally {
     await prisma.$disconnect();
   }
--- src/routes/clientRoutes.ts
+++ src/routes/clientRoutes.ts
@@ -1,12 +1,14 @@
 import express from "express";
-import { createClient, listClients, findClient } from "../controllers/clientController";
+import { createClient, listClients, findClient, deleteClient } from "../controllers/clientController";
 
 const clientRoutes = express.Router();
 
 clientRoutes.post("/create", createClient);
 
-clientRoutes.get("/list-clients", listClients )
+clientRoutes.get("/list-all", listClients )
 
 clientRoutes.get("/find-client/:id", findClient )
+
+clientRoutes.delete("/delete/:id", deleteClient )
 
 export default clientRoutes;
