d3631f09becbe4032f0708f9a240a469e8cbfe89
oiviana <vianalucas80@gmail.com>
2024-06-30 21:48:59 -0300

    feat: adding address to app middleware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
 import express from 'express'
 import clientRoutes from './routes/clientRoutes'
+import addressRoutes from './routes/addressRoutes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-  });
-
 app.use('/clients', clientRoutes);
+app.use('/address', addressRoutes);
 
 export default app;
