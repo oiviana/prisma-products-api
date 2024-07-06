89beb28b90e6a1e213d2b894b74aecc23e514620
oiviana <vianalucas80@gmail.com>
2024-07-06 15:58:55 -0300

    feat: adding product in the middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import clientRoutes from './routes/clientRoutes'
 import addressRoutes from './routes/addressRoutes';
+import productRoutes from './routes/productRoutes';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/clients', clientRoutes);
 app.use('/address', addressRoutes);
+app.use('/product', productRoutes)
 
 export default app;
