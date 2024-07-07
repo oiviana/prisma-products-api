b096dd1d7d566f058320f01a9a452e249b4be6b8
oiviana <vianalucas80@gmail.com>
2024-07-07 16:12:33 -0300

    fix: changing console

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import app from "./app";
 
 const port = process.env.SERVER_PORT ?? 3000;
 
-app.listen(port, () => console.log(`server is running on port ${port}`));
+app.listen(port, () => console.info(`server is running on port ${port}`));
