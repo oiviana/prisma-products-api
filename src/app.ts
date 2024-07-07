fd25b7963adfdadc4e2a096dc50acd005833487f
oiviana <vianalucas80@gmail.com>
2024-07-07 17:07:50 -0300

    feat: security middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,25 @@ import express from 'express'
 import clientRoutes from './routes/clientRoutes'
 import addressRoutes from './routes/addressRoutes';
 import productRoutes from './routes/productRoutes';
+import { Request, Response, ErrorRequestHandler, NextFunction } from "express";
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
 
 app.use('/clients', clientRoutes);
 app.use('/address', addressRoutes);
 app.use('/product', productRoutes)
+
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      console.error(err.stack);
+      res.status(500).send('Algo deu errado!');
+    } else {
+      res.status(500).send('Algo deu errado!');
+    }
+  });
 
 export default app;
