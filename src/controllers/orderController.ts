ef258c8d59c9d4ab98a3a743282fe68c394bbb05
oiviana <vianalucas80@gmail.com>
2024-07-14 20:48:45 -0300

    test: order crud

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -6,38 +6,106 @@ const prisma = new PrismaClient();
 
 const orderItemSchema = z.object({
   productId: z.string(),
-  orderId: z.string(),
   itemQuantity: z.number(),
   itemTotalPrice: z.number(),
+  orderId: z.string().optional(),
 });
+
+const ItemsSchema = z.array(orderItemSchema);
 
 const orderSchema = z.object({
   orderStatus: z.string(),
+  orderNumber: z.string(),
   clientId: z.string(),
-  orderId: z.string(),
-  productId: z.string(),
-  orderItems: z.array(orderItemSchema),
 });
 
 // POST
 export const createOrder = async (req: Request, res: Response) => {
-  const { orderStatus, clientId, orderItems } = orderSchema.parse(req.body);
+  const { orderStatus, clientId, orderNumber } = orderSchema.parse(req.body);
+
   try {
+    // Primeiro, cria a ordem
     await prisma.order.create({
       data: {
+        orderNumber,
         orderStatus,
         clientId,
+      },
+    });
+
+    // Retorna a ordem criada e os itens do pedido
+    res.status(201).json("Pedido criado");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: `Erro ao cadastrar: ${error}` });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const createOrderItems = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const items = ItemsSchema.parse(req.body);
+
+  const itemsWithOrderId = items.map((item) => ({
+    ...item,
+    orderId: id,
+  }));
+
+  try {
+
+    const hasOrder = await prisma.order.findUnique({
+      where:{
+        orderId: id
+      }
+    })
+
+    if(!hasOrder){
+      return  res.status(404).json("Não há pedido com esse id");
+    }
+    // Primeiro, cria a ordem
+    await prisma.orderItem.createMany({
+      data: itemsWithOrderId,
+    });
+
+    // Retorna a ordem criada e os itens do pedido
+    res.status(201).json("Itens adicionados");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: `Erro ao cadastrar: ${error}` });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// GET
+export const findOrderWithItems = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const order = await prisma.order.findUnique({
+      where: {
+        orderId: id,
+      },
+      include: {
         orderItems: {
-          createMany: {
-            data: orderItems,
+          select: {
+            itemQuantity: true,
+            itemTotalPrice: true,
+            product: true,
           },
         },
       },
     });
-    res.status(201).json("Cliente cadastrado com sucesso!");
+
+    if (!order) {
+      return res.status(404).json({ error: "Pedido não encontrado" });
+    }
+
+    res.json(order);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: `Erro ao cadastrar: ${error}` });
+    console.error("Erro ao buscar pedido:", error);
+    res.status(500).json({ error: `Erro ao buscar pedido: ${error}` });
   } finally {
     await prisma.$disconnect();
   }
--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,6 +1,10 @@
 import express from "express";
-import { createOrder } from "../controllers/orderController";
+import { createOrder, createOrderItems, findOrderWithItems } from "../controllers/orderController";
 
 const orderRoutes = express.Router();
 
 orderRoutes.post("/create", createOrder);
+orderRoutes.post("/create-items/:id", createOrderItems);
+orderRoutes.get("/list-with-items/:id", findOrderWithItems);
+
+export default orderRoutes;
