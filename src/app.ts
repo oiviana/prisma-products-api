fa330db4bdf86c992834614a6d27db6d978e6f63
oiviana <vianalucas80@gmail.com>
2024-07-14 20:48:34 -0300

    refactor: imports

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express from 'express'
+import express, { Request, Response, NextFunction }  from 'express'
 import clientRoutes from './routes/clientRoutes'
 import addressRoutes from './routes/addressRoutes';
 import productRoutes from './routes/productRoutes';
-import { Request, Response, ErrorRequestHandler, NextFunction } from "express";
-import helmet from 'helmet';
 import stockRoutes from './routes/stockRoutes';
+import orderRoutes from './routes/orderRoutes';
+import helmet from 'helmet';
 
 const app = express();
 
@@ -15,6 +15,7 @@ app.use('/clients', clientRoutes);
 app.use('/address', addressRoutes);
 app.use('/product', productRoutes)
 app.use('/stock', stockRoutes)
+app.use('/order', orderRoutes)
 
 app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
